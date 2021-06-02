988e53d11bdf38d242105f4f96cab923789c2d0c
rimichaeru <conceptual@protonmail.com>
2021-06-02 15:56:24 +0100

    Added dynamic slider for project display

---
+++ src/dev/dist/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+new Splide(".splide", {
+  // width: "80%",
+  padding: {
+    left: "10rem",
+    right: "10rem"
+  },
+  cover: true,
+  height: "20rem",
+  focus: "center",
+  perPage: 3,
+  rewind: true
+}).mount();
--- src/dev/main.js
+++ src/dev/main.js
@@ -1,2 +1,23 @@
-new Splide( '.splide' ).mount();
+new Splide( ".splide", {
+	gap: "6rem",
+	padding: {
+		left: "10rem",
+		right: "10rem"
+	},
+	// cover: true,
+	type: "loop",
+	height: "40rem",
+	// fixedWidth: "500px", 
+	focus: "center",
+	perPage: 3,
+	rewind : true,
+	autoplay: true,
+	interval: 10000,
+	pauseOnHover: true,
+	pauseOnFocus: true,
+	resetProgress: true,
+	drag: true,
+	flickMaxPages: 1,
+
+}  ).mount();
 
